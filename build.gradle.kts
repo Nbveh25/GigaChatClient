8b3d75cf13367c6ca5a082fc77ac47a8cfb73337
Бикмухаметов Тимур Ильдарович TiIBikmukhametov <tiibikmukhametov@stud.kpfu.ru>
2026-03-20 23:05:53 +0300

    refactor: versionCode, versionName move to root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
+import org.gradle.internal.extensions.core.extra
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+
+private val versionMajor = 1
+private val versionMinor = 0
+
+val versionName by extra(initialValue = "$versionMajor.$versionMinor")
+val versionCode by extra(initialValue = versionMajor * 1000 + versionMinor * 10)
