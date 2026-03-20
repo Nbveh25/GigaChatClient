23b152946ee523765ea2e9fbd1ba546aa18fd462
Бикмухаметов Тимур Ильдарович TiIBikmukhametov <tiibikmukhametov@stud.kpfu.ru>
2026-03-21 02:52:50 +0300

    refactor: Google services plugin add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
 
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
+
+    alias(libs.plugins.google.services)
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ plugins {
 
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
+
+    alias(libs.plugins.google.services) apply false
 }
 
 private val versionMajor = 1
